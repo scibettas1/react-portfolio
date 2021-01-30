1cc88e1634b47840ce9799b7075f2030f6f7e5b6
Sam Scibetta <sammieeccentric@gmail.com>
2021-01-29 19:27:35 -0500

    corrected syntax errors and succsufully rendered the home page with images and css. need to research how to embed google fonts

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,75 +1,78 @@
 import React from "react";
 import "../../styles/style.css"
+import profile from "../../images/profile.jpg"
+import placeholder from "../../images/placholder350.jpg"
 
 function Home() {
   return (
     <div>
-      <div class="hero">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-7"></div>
-            <div class="col-md-5">
-              <img src="images/IMG_0030.JPG" class="img-fluid rounded-circle profile-margin" alt="profile photo of sam scibetta"/>
+      <div className="hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7"></div>
+            <div className="col-md-5">
+              <img src={profile} className="img-fluid rounded-circle profile-margin" alt="profile sam scibetta"/>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-5">
+          <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-5">
               <h1>Sam Scibetta<br />
                 Graphic &amp; Web<br />
                 Designer</h1>
             </div>
 
-            <div class="col-md-5">
+            <div className="col-md-5">
               <p>Sam Scibetta graduated from William Paterson University with a BFA in Fine Art, and also attended the Art
                 Institute of Pittsburgh.
                 She is currenlty studying full-stack web development at Rutgers Coding Bootcamp.
                 Sam has explored various mediums including graphic &amp; web design, musical performance &amp; composition, and
                 creative writing.
-                She has 10+ years of graphic design experience, 15 years of classical
+                She has 10+ years of graphic design experience, 15 years of classNameical
                 voice training, and is the current singer for the symphonic metal band, Infinitus Mortus.</p>
             </div>
           </div>
         </div>
       </div>
-      <div class="info1">
-        <div class="container">
+      <div className="info1">
+        <div className="container">
           <h3>WEB DEVELOPMENT</h3>
-          <div class="row">
-            <div class="col-md-5">
+          <div className="row">
+            <div className="col-md-5">
               <h4>HTML | CSS | JAVASCRIPT | ANIMATIONS | UI/UX | API | SQL | MONGODB | REACT | HEROKU | CPANEL | GIT |
                 GITHUB | WORDPRESS</h4>
           </div>
 
-          <div class="col-md-7">
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <button type="button" class="btn view">View More</button>
+          <div className="col-md-7">
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <button type="button" className="btn view">View More</button>
             </div>
         </div>
       </div>
 
-      <div class="info2">
-        <div class="container">
+      <div className="info2">
+        <div className="container">
           <h2>GRAPHIC DESIGN</h2>
-          <div class="row">
-            <div class="col-md-5">
+          <div className="row">
+            <div className="col-md-5">
               <h5>PHOTOSHOP | ADOBE ILLUSTRATOR | INDESIGN | TYPOGRAPHY</h5>
             </div>
 
-            <div class="col-md-7">
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <img src="images/placholder350.jpg" class="rounded img-thumbnail thumb"/>
-              <button type="button" class="btn view">View More</button>
+            <div className="col-md-7">
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <img src={placeholder} className="rounded img-thumbnail thumb" alt=""/>
+              <button type="button" className="btn view">View More</button>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './uikit/css/uikit.css';
-import './uikit/js/uikit.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
