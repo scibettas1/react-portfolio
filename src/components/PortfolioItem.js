e60971bbc7d08a9ce78a57dd82568eb399d4044f
Sam Scibetta <sammieeccentric@gmail.com>
2021-02-03 00:45:32 -0500

    images are working in the modalsgit add .git add . just need to get the button links working

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -3,37 +3,18 @@ import "../styles/style.css"
 import { Button, Modal } from "react-bootstrap"
 
 
-function PortfolioModal(props) {
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <img src={props.image} className="img-fluid" alt={props.alt} />
-            </Modal.Header>
-            <Modal.Body>
-                {props.description}
-                <Button src={props.link} />
-                <Button src= {props.repo} />
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-        </Modal>
-    );
-}
 
 function ModalLaunch(props) {
     const [modalShow, setModalShow] = React.useState(false);
+    const portfolioItem = props;
+    console.log(portfolioItem)
+
 
     return (
         <>
 
             <img src={props.image} alt={props.alt} className="rounded thumb" onClick={() => setModalShow(true)}/>
-
+            
 
             <PortfolioModal
                 show={modalShow}
@@ -41,6 +22,30 @@ function ModalLaunch(props) {
             />
         </>
     );
+
+    function PortfolioModal(props) {
+        return (
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <img src={portfolioItem.image} className="img-fluid" alt={portfolioItem.alt} />
+                </Modal.Header>
+                <Modal.Body>
+                    {portfolioItem.description}
+                    <Button src={portfolioItem.link} />
+                    <Button src= {portfolioItem.repo} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
 }
  export default ModalLaunch
 
